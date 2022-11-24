1c5d81a3f9f198a8f88515e2fa81122207b21675
Daniel Murphy <daniel.murphy@deptagency.com>
2022-11-24 10:48:57 +0000

    simple login password screen

---
+++ screens/LoginScreen.js
@@ -0,0 +1,40 @@
+import { StyleSheet, Text, TextInput, View } from 'react-native'
+import React from 'react'
+import { KeyboardAvoidingView } from 'react-native-web'
+
+const LoginScreen = () => {
+  return (
+    <KeyboardAvoidingView
+    style={styles.container}
+    behavior="padding"
+    >
+      <View style={styles.inputContainer}>
+        <TextInput
+        placeholder='Email'
+        // value={}
+        // onChangeText={text => }
+        style={styles.input}  
+        />
+
+        <TextInput
+        placeholder='Password'
+        // value={}
+        // onChangeText={text => }
+        style={styles.input}  
+        secureTextEntry
+        />
+
+      </View>
+    </KeyboardAvoidingView>
+  )
+}
+
+export default LoginScreen
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+    }
+})
