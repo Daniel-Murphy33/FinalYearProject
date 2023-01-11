d3c7d1b719131f99b19bd2f24ae1da1f249b1e44
Daniel Murphy <daniel.murphy@deptagency.com>
2023-01-11 11:25:47 +0000

    exercise screen for home workouts

---
+++ screens/HomeExerciseScreen/JumpingJacks.js
@@ -0,0 +1,14 @@
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const JumpingJacks = () => {
+  return (
+    <SafeAreaView>
+      <Text>JumpingJacks</Text>
+    </SafeAreaView>
+  )
+}
+
+export default JumpingJacks
+
+const styles = StyleSheet.create({})
